2d141efa8fa09411347dd9fb6fc8d484de3b1adb
Haffner32 <adenchan01@gmail.com>
2025-08-24 11:21:31 +0800

    Level-1

--- src/main/java/Arvee.java
+++ src/main/java/Arvee.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Arvee {
     public static void main(String[] args) {
         System.out.println("Hello! I'm ARVEE" +
-                "\n" + "What can I do for you?" +
-                "\n" + "Bye. Hope to see you again soon!");
+                "\n" + "What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine().trim();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
