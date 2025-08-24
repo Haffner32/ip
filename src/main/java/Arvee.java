b0c57dc01aad0fb771c1a765e7a23e7fcd2a1d53
Haffner32 <adenchan01@gmail.com>
2025-08-24 11:09:00 +0800

    Level-0

---
+++ src/main/java/Arvee.java
@@ -0,0 +1,7 @@
+public class Arvee {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm ARVEE" +
+                "\n" + "What can I do for you?" +
+                "\n" + "Bye. Hope to see you again soon!");
+    }
+}
