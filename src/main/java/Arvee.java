6ade32229cf26eb4860dd1818aedfeddea6f9b32
Haffner32 <adenchan01@gmail.com>
2025-08-25 14:58:19 +0800

    Level-5

--- src/main/java/Arvee.java
+++ src/main/java/Arvee.java
@@ -8,57 +8,88 @@ public class Arvee {
                 "\n" + "What can I do for you?");
         Scanner sc = new Scanner(System.in);
         ArrayList<Task> items = new ArrayList<>();
-        while (true) {
+        while (sc.hasNextLine()) {
             String input = sc.nextLine().trim();
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (input.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                int size = items.size();
-                for (int i = 0; i < size; i++) {
-                    String out = String.format("%s. %s", i + 1, items.get(i));
-                    System.out.println(out);
-                }
-            } else if (input.startsWith("mark ")) {
-                int index = Integer.valueOf(input.substring(5)) - 1;
-                Task change = items.get(index);
-                change.setDone(true);
-                items.set(index, change);
-                System.out.println(String.format("Nice! I've marked this task as done:\n %s", change));
-            } else if (input.startsWith("unmark ")) {
-                int index = Integer.valueOf(input.substring(7)) - 1;
-                Task change = items.get(index);
-                change.setDone(false);
-                items.set(index, change);
-                System.out.println(String.format("Ok, I've marked this task as not done yet:\n %s", change));
             }
-            else {
-                Task next = null;
+
+            try {
+                if (input.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    int size = items.size();
+                    for (int i = 0; i < size; i++) {
+                        String out = String.format("%s. %s", i + 1, items.get(i));
+                        System.out.println(out);
+                    }
+                    continue;
+                }
+
+                if (input.startsWith("mark ")) {
+                    int index = Integer.valueOf(input.substring(5)) - 1;
+                    Task change = items.get(index);
+                    change.setDone(true);
+                    items.set(index, change);
+                    System.out.println(String.format("Nice! I've marked this task as done:\n %s", change));
+                    continue;
+                }
+
+                if (input.startsWith("unmark ")) {
+                    int index = Integer.valueOf(input.substring(7)) - 1;
+                    Task change = items.get(index);
+                    change.setDone(false);
+                    items.set(index, change);
+                    System.out.println(String.format("Ok, I've marked this task as not done yet:\n %s", change));
+                    continue;
+                }
                 if (input.startsWith("todo")) {
-                    String task = input.substring(5);
-                    next = new ToDoTask(task);
-                } else if (input.startsWith("deadline")) {
-                    String rest = input.substring(9).trim();
+                    String task = requireNonBlank(input.substring(4).trim());
+                    Task next = new ToDoTask(task);
+                    items.add(next);
+                    System.out.println(String.format("Got it. I've added this task:\n" +
+                            " %s\n" +
+                            "Now you have %s tasks in the list.", next, items.size()));
+                    continue;
+                }
+                if (input.startsWith("deadline")) {
+                    String rest = requireNonBlank(input.substring(8).trim());
                     String[] parts = rest.split("/by", 2);
                     String task = parts[0];
                     String deadline = parts[1];
-                    next = new Deadlines(task, deadline);
-                } else if (input.startsWith("event")) {
-                    String rest = input.substring(6).trim();
+                    Task next = new Deadlines(task, deadline);
+                    items.add(next);
+                    System.out.println(String.format("Got it. I've added this task:\n" +
+                            " %s\n" +
+                            "Now you have %s tasks in the list.", next, items.size()));
+                    continue;
+                }
+                if (input.startsWith("event")) {
+                    String rest = requireNonBlank(input.substring(5).trim());
                     String[] parts = rest.split("/from", 2);
                     String task = parts[0].trim();
                     String[] timeParts = parts[1].split("/to",2);
                     String start = timeParts[0];
                     String end = timeParts[1];
-                    next = new Event(task, start, end);
+                    Task next = new Event(task, start, end);
+                    items.add(next);
+                    System.out.println(String.format("Got it. I've added this task:\n" +
+                            " %s\n" +
+                            "Now you have %s tasks in the list.", next, items.size()));
+                    continue;
                 }
-                items.add(next);
-                System.out.println(String.format("Got it. I've added this task:\n" +
-                        " %s\n" +
-                        "Now you have %s tasks in the list.", next, items.size()));
+                throw new UnknownCommandException();
+            } catch (EmptyDescriptionException e) {
+                System.out.println(e.getMessage());
+            } catch (UnknownCommandException e) {
+                System.out.println("Sorry, I don't recognize that command");
             }
         }
     }
 
+    public static String requireNonBlank(String desc) throws EmptyDescriptionException {
+        if (desc == null || desc.isBlank()) throw new EmptyDescriptionException(
+                "OOPS!!! The description of a task cannot be empty.");
+        return desc;
+    }
 }
