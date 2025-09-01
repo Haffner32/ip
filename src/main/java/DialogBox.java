f3345e8a09344e0ab97d66d092e7f562f6439702
Haffner32 <adenchan01@gmail.com>
2025-09-01 17:37:08 +0800

    add image and message for Arvee and user

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,25 +1,72 @@
+import java.util.Collections;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
+
 /**
  * represents the graphical interface for the dialogue box
  */
 public class DialogBox extends HBox {
-    private final Label text;
-    private final ImageView displayPicture;
+    private final Label text = new Label();
+    private final ImageView displayPicture = new ImageView();
 
     /**
      *  Constructor for the dialogue box
      * @param s message
      * @param i image
      */
-    public DialogBox(String s, Image i) {
-        text = new Label(s);
-        displayPicture = new ImageView(i);
-        getChildren().addAll(text, displayPicture);
+    private DialogBox(String s, Image i) {
+        super(8);
+        text.setText(s);
+        text.setWrapText(true);
+
+        displayPicture.setImage(i);
+        displayPicture.setFitWidth(40);
+        displayPicture.setFitHeight(40);
+        displayPicture.setPreserveRatio(true);
+
         setAlignment(Pos.TOP_RIGHT);
+        setPadding(new Insets(8, 10, 8, 10));
+        getChildren().addAll(text, displayPicture);
+    }
+
+    /**
+     *  flips the dialogue to the left for Arvee
+     */
+    private void flip() {
+        setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> nodes = FXCollections.observableArrayList(getChildren());
+        Collections.reverse(nodes);
+        getChildren().setAll(nodes);
+    }
+
+    /**
+     * factory method for user dialog (right aligned)
+     * @param msg message to be sent
+     * @param img avatar
+     * @return the Dialogue box
+     */
+    public static DialogBox getUserDialog(String msg, Image img) {
+        return new DialogBox(msg, img);
+    }
+
+    /**
+     * factory method for Arvee
+     * @param msg message to be set
+     * @param img avatar
+     * @return the dialogueBox
+     */
+    public static DialogBox getArveeDialog(String msg, Image img) {
+        DialogBox db = new DialogBox(msg, img);
+        db.flip();
+        return db;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,65 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-private final Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-private final Image rvImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
 /**
  * The main GUI logic
  */
 public class Main extends Application {
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+
+    private final Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private final Image rvImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!");
-        Scene scene = new Scene(helloWorld);
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox(8);
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        scrollPane.setContent(dialogContainer);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPadding(new Insets(10));
+        userInput.setPromptText("Type a message…");
+
+        AnchorPane root = new AnchorPane();
+        root.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        // anchor the components (so they resize nicely)
+        AnchorPane.setTopAnchor(scrollPane, 8.0);
+        AnchorPane.setLeftAnchor(scrollPane, 8.0);
+        AnchorPane.setRightAnchor(scrollPane, 8.0);
+        AnchorPane.setBottomAnchor(scrollPane, 56.0);
+
+        AnchorPane.setLeftAnchor(userInput, 8.0);
+        AnchorPane.setRightAnchor(userInput, 84.0);
+        AnchorPane.setBottomAnchor(userInput, 8.0);
+
+        AnchorPane.setRightAnchor(sendButton, 8.0);
+        AnchorPane.setBottomAnchor(sendButton, 8.0);
+        sendButton.setPrefWidth(68);
+
+        dialogContainer.getChildren().add(
+                DialogBox.getArveeDialog("Hello!", rvImage)
+        );
+
+        dialogContainer.heightProperty().addListener((obs, oldV, newV) -> scrollPane.setVvalue(1.0));
+
+        Scene scene = new Scene(root, 400, 600);
+        stage.setTitle("Arvee");
         stage.setScene(scene);
         stage.show();
     }
